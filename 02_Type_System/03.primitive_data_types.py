d9a587b893f022df41b42b9229ceedc3dbd941de
Mehmet Ugur <mehmetbugur@hotmail.com>
2022-11-05 14:42:05 +0800

    add 03.primitive_data_types.py

---
+++ 02_Type_System/03.primitive_data_types.py
@@ -0,0 +1,21 @@
+# --------------------
+# Primitive Data Types
+# --------------------
+
+# 1. Strings    "WeDo4Fun"
+# 2. Integers   Priamry numbers i.e. 159
+# 3. Floats     Numbers with decimals i.e 3.1415
+# 4. Boolean    True / False 
+
+# --------------------
+
+my_string = "WeDo4Fun"
+my_int = 159
+my_float = 3.1415
+my_bool = True
+
+print(my_string)
+print(my_int)
+print(my_float)
+print(my_bool)
+
