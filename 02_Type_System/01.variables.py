6056a212fc9158a63a464d049df487e97ddaa772
Mehmet Ugur <mehmetbugur@hotmail.com>
2022-11-05 08:42:18 +0800

    add 01.variables.py' '

---
+++ 02_Type_System/01.variables.py
@@ -0,0 +1,14 @@
+# Louis is going for a party, hehas got balloons, crackers and hats
+# but only single box
+
+# declaring new variable called 'box' and puts balloons instead
+box = 'balloons'
+print (box)
+
+# Lousi changes mind and wants to pack in cracker instead:
+box = 'crackers'
+print (box)
+
+# Louis now wants to pack hats:
+box = 'hats'
+print (box)
