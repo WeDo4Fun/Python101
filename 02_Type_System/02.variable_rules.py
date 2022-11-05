4ae7e10056f3088ac8f76f38668878f492f48bd8
Mehmet Ugur <mehmetbugur@hotmail.com>
2022-11-05 09:18:49 +0800

    add 02.variable_rules.py

---
+++ 02_Type_System/02.variable_rules.py
@@ -0,0 +1,35 @@
+# -----------------------------------
+# Common Variable Declaration Formats
+# -----------------------------------
+
+# try to be consistent in your code whcih convention you use
+# the most broadly used convention are 
+# for declaring variables or naming function use style_snake_case 
+# for declarign a name of CLASS we use StylePascalCase
+
+styleCamelCase = "variableName"
+StylePascalCase = "VariableName"    # declaring CLASS
+style_snake_case = "variable_name"  # standard variabe or naming function
+STYLEUPPERCASE = "VARIABLENAME"     # this is used as a CONSTANT
+_supper_secret = "_variable_name"   # PRIVATE variable
+styleWithNumber1 = "variableName4"
+style_with_number_2 = "variable_name_3"
+
+print(styleCamelCase)
+print(StylePascalCase)
+print(style_snake_case)
+print(STYLEUPPERCASE)
+print(_supper_secret)
+print(styleWithNumber1)
+print(style_with_number_2)
+
+# --------------------------------------
+# Following styles are NOT valid formats
+# --------------------------------------
+
+# You can delete below at the bginnig the '#' to see error msgs
+# 3_style_number = "Variable cannot start with number"
+# style-with-hypen = "Variable cannot contain HYPEN"
+# styleWith? = "Variables cannot contain special characeters"
+# $styleStart = "Variables cannot start with special characters"
+# style With Space = "Variables cannot contain spece"
