72bd4e525fb36957774b8aa428b0979a0ba0cd42
Mehmet Ugur <mehmetbugur@hotmail.com>
2022-11-05 15:02:27 +0800

    add 04.string_formatting.py

---
+++ 02_Type_System/04.string_formatting.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------
+# String Formatting for getting desired string outputs
+# ----------------------------------------------------
+
+name = "John"
+age = 21
+location = "Carlini Base"
+job = "pilot"
+
+print(f"Hello {name}")
+print("Hello", name)
+
+print(f"{name} is {age} years old.")
+print(name, "is", age, "years old.")
+
+print(f"{name} lives in {location}")
+print(name, "lives in", location)
+
+print(f"{name} is a {job}")
+print(name, "is a", job)
