0b89dc7db03e71ca785707d227e80210008d523a
Mehmet Ugur <mehmetbugur@hotmail.com>
2022-12-02 22:42:03 +0800

    add edited 02.and_or.py

--- 03_Flow_Control/02.and_or.py
+++ 03_Flow_Control/02.and_or.py
@@ -1,3 +1,16 @@
 """
 Expanding if / elif / else with and/or
 """
+my_int_variable: int = 20
+my_str_variable: str = "String"
+
+# --------------------------------
+# And / Or Statements
+# --------------------------------
+
+if my_int_variable == 20 and my_str_variable == "String":
+    print("Both conditions are met")
+elif my_int_variable < 20 or my_str_variable == "String":
+    print("One of the conditions are met")
+
+time_on_video: str = "1:06:22"
