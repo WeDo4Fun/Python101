9df09e171b8393bb730a1c140211144c0248f260
Mehmet Ugur <mehmetbugur@hotmail.com>
2022-12-04 14:18:51 +0800

    edit 02.and_or.py

--- 03_Flow_Control/02.and_or.py
+++ 03_Flow_Control/02.and_or.py
@@ -1,16 +1,35 @@
 """
 Expanding if / elif / else with and/or
 """
-my_int_variable: int = 20
-my_str_variable: str = "String"
+
+inp_number: int = int(input("Please enter a number between 0-30: "))
+inp_string: str = input("Please choose a color Reg / Green / Blue (R/G/B): ")
+color_choice=""
 
 # --------------------------------
 # And / Or Statements
 # --------------------------------
 
-if my_int_variable == 20 and my_str_variable == "String":
-    print("Both conditions are met")
-elif my_int_variable < 20 or my_str_variable == "String":
-    print("One of the conditions are met")
+if inp_string.lower() == "r":
+    color_choice="RED"
+elif inp_string.lower() == "g":
+    color_choice = "GREEN"
+elif inp_string.lower == "b":
+    color_choice = "BLUE"
+elif inp_string.lower != "r" or "g" or "b":
+    color_choice = ("Unknown")
+
+if inp_number == 20:
+    print(f"Your number is 20, color is {color_choice}")
+elif inp_number < 20 and inp_number >= 0:
+    print(f"Your number is smaller than 20, color is {color_choice}")
+elif inp_number > 20 and inp_number < 30:
+    print(f"Your number is bigger than 20, color is {color_choice}")
+else:
+    print (f"the number you chose is in not betwen 0 and 30!, color is {color_choice}.")
+
+
+
+
 
 time_on_video: str = "1:06:22"
